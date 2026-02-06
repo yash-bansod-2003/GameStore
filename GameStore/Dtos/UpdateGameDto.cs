5214c340cc11d8ba7c4a6f0144ef80eaf7f83eeb
yashbansod <yashbansodgit@gmail.com>
2026-02-06 07:06:16 +0530

    Add validation attributes to CreateGameDto and UpdateGameDto for Title, Genre, and Price

--- GameStore/Dtos/UpdateGameDto.cs
+++ GameStore/Dtos/UpdateGameDto.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Dtos;
 
 public record class UpdateGameDto(
-  String Title,
-  String Genre,
-  Decimal Price,
+  [Required][StringLength(50)] string Title,
+  [Required][StringLength(20)] string Genre,
+  [Range(1, 100)] decimal Price,
   DateOnly ReleaseDate
 );
